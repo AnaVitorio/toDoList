d026d9d25db04bec2b403fa98a6609fc998d95d1
Ana Carolina <anavitorio.p@gmail.com>
2023-11-20 22:04:23 -0300

    criando painel de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AdicionarTarefa } from "./componets/AdicionarTarefa";
 import { Header } from "./componets/Header";
+import { PainelTarefas } from "./componets/PainelTarefas";
 
 function App() {
   return (
     <div>
       <Header />
       <AdicionarTarefa/>
+      <PainelTarefas/>
     </div>
   );
 }
---
+++ src/componets/PainelTarefas.tsx
@@ -0,0 +1,27 @@
+import styles from "./PainelTarefas.module.css";
+import cliboard from "../assets/Clipboard.png";
+
+export function PainelTarefas() {
+  return (
+    <div className={styles.painelTarefas}>
+      <header>
+        <div className={styles.criadas}>
+          <span>Tarefas criadas</span>
+          <span className={styles.contador}>0</span>
+        </div>
+        <div className={styles.concluidas}>
+          <span>Concluídas</span>
+          <span className={styles.contador}>0</span>
+        </div>
+      </header>
+
+      <div className={styles.painelTarefasCriadas}>
+        <img src={cliboard} />
+        <span>
+          <strong>Você ainda não tem tarefas cadastradas</strong>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </span>
+      </div>
+    </div>
+  );
+}
